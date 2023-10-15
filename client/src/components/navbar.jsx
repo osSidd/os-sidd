e6a1a269fbffa4d49231bec1a1c2893ab336c502
Osama <107386980+osSidd@users.noreply.github.com>
2023-10-15 12:20:57 +0530

    navbar style
    added pseudo element with hover class

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -4,9 +4,39 @@ import MenuIcon from '@mui/icons-material/Menu'
 export default function Navbar(){
 
     const pages = ['home', 'about', 'projects', 'experience']
+    const btnProps = {
+        ml: 2,
+        textTransform: 'capitalize',
+        fontSize:18,
+        fontWeight:600,
+        color:'#555',
+        letterSpacing: 1,
+        transition:'color 0.35s ease-out',
+        "&:hover":{
+            backgroundColor:'#fefefe',
+            color:'#34c'
+        },
+    }
+    const linksProps = {
+        position:'relative',
+        "&::after":{
+            content:'""',
+            position: 'absolute',
+            bottom:5,
+            left:8,
+            backgroundColor: '#34c',
+            width:"0%",
+            height:3,
+            borderRadius:1,
+            transition:'width 0.35s ease-out'
+        },
+        "&:hover::after":{
+            width:'90%', 
+        }   
+    }
 
     return(
-        <AppBar sx={{bgcolor:'#fefefe'}} position='static'>
+        <AppBar sx={{bgcolor:'#fefefe', mt:2}} elevation={false} position='static'>
             <Container maxWidth="xl">
                 <Toolbar>
                     {/* <Box sx={{display:{xs:'flex', md:'none'}}}>
@@ -17,12 +47,17 @@ export default function Navbar(){
                     <Box ml='auto'>
                         {
                             pages.map(page => (
-                                <Button key={page}>
+                                <Button key={page} sx={[btnProps, linksProps]}>
                                     {page}
                                 </Button>
                             ))
                         }
-                        <Button variant='outlined'>get in contact</Button>
+                        <Button 
+                            variant='outlined' 
+                            sx={{...btnProps, borderWidth:2, borderColor:'#555', "&:hover":{borderWidth:2, backgroundColor:'#fefefe', borderColor:'#34c', color:"#34c"}}}
+                        >
+                            get in touch
+                        </Button>
                     </Box>
                 </Toolbar>
             </Container>
